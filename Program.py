f3160eb79cbb3f8fecba2e26161203310d8179e0
yun0509 <yun0509av@bitbucket.org>
2018-08-22 14:21:13 -0400

    Modify functions
    Configure urllib in the function

--- Program.py
+++ Program.py
@@ -1,13 +1,19 @@
+from urllib.request import Request, urlopen
+
+
 def read_text(file):
 	archive = open(file)
 	text = archive.read()
-	print(text)
+	check_profanity(text)
 	archive.close()
 
 def check_profanity(text_to_check):
-	connection = urllib.urlopen("http://www.wdyl.com/profanity?q=" + text_to_check)
+	q = Request("http://www.wdylike.appspot.com/?q=" + text_to_check)
+	q.add_header('User-Agent', 'Mozilla/5.0')
+	connection = urlopen(q)
 	output = connection.read()
+	connection.close()
 	print(output)
-	
+
 file= r"C:\Users\Cincinnatus\repositorios\lector\text.txt"
 read_text(file)

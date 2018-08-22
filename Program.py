399441a6800584f514b2d1dba45349433f7ee0d9
yun0509 <yun0509av@bitbucket.org>
2018-08-22 12:19:27 -0400

    Add a function check_profanity
    Take a text and verify with the web page www.wdyl.com if contect a curse
    word

--- Program.py
+++ Program.py
@@ -4,6 +4,10 @@ def read_text(file):
 	print(text)
 	archive.close()
 
-
+def check_profanity(text_to_check):
+	connection = urllib.urlopen("http://www.wdyl.com/profanity?q=" + text_to_check)
+	output = connection.read()
+	print(output)
+	
 file= r"C:\Users\Cincinnatus\repositorios\lector\text.txt"
 read_text(file)
